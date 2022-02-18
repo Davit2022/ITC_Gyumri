c96679e8e1e4dbcef4c792ca9822f168786be108
Davit2022 <davit.panosyan.im@gmail.com>
2022-02-18 13:45:20 +0400

    Added file

--- python/finde_words/finde_words.py
+++ python/finde_words/finde_words.py
@@ -1,7 +1,6 @@
 import random
 from posixpath import split
 
- 
 name = input("What is your name? ")
  
 print("Good Luck ! ", name)
@@ -9,55 +8,32 @@ print("Good Luck ! ", name)
 file = open("file.txt")
 words = file.readline()
 words = words.split(", ")
- 
 word = random.choice(words)
 
- 
 print("Guess the characters")
- 
 guesses = ''
- 
 quantity = 3
- 
- 
 while quantity > 0:
-     
     failed = 0
-     
     for i in word:
-         
-  
         if i in guesses:
-            print(i, end=" ")
-             
+            print(i, end=" ")    
         else:
             print("_")
-             
             failed += 1
-             
- 
     if failed == 0:
-    
         print("\nYou Win")
-         
-
         print("The word is: ", word)
         break
-     
-  
+    
     print()
     guess = input("guess a character:")
-
     guesses += guess
-     
+    
     if guess not in word:
-         
         quantity -= 1
-         
         print("Wrong")
-         
         print("You have", + quantity, 'more guesses')
-         
          
         if quantity == 0:
             print("You Loose")
