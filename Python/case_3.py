8b4069c68e218a64c5739f7d376cd2891d198c43
Heghine2021 <heghine.sarkisyan@realschool.am>
2022-02-22 01:59:04 +0400

    Delet file

--- Python/case_3.py
+++
@@ -1,7 +0,0 @@
-def sortString(str) :
-	str = ''.join(sorted(str))
-	print(str)
-
-s = "aa3 da2 aa1 derfde"
-sortString(s)
-
--- Python/paskal.py
+++
@@ -1,7 +0,0 @@
-n = 8
-
-for i in range(n):
-	print(' '*(n-i), end='')
-
-	print(' '.join(map(str, str(11**i))))
-
