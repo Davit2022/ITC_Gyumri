93b6942b71adc8a93f0bcfd46acafeee5cd2612d
Davit2022 <davit.panosyan.im@gmail.com>
2022-02-21 14:07:11 +0400

    Added file

--- python/OOP/car.py
+++ python/OOP/car.py
@@ -1,20 +1,12 @@
+from pickle import TRUE
+from tokenize import Number
+
+
 class Car:
-    __engine = 0
-    __doorsCount = 0
-    __color = 0
     def __init__(self, engine, doorsCount, color_car):
         self.__engine = engine  
         self.__doorsCount = doorsCount
         self.__color = color_car
-
-    def set_color(self, color_car):
-        self.__color = color_car   
-        
-    def set_engine(self, engine):
-        self.__engine = engine
-
-    def set_doorsCount(self, doorsCount):
-        self.__doorsCount = doorsCount
 
     def get_color(self):
         return self.__color
@@ -31,14 +23,18 @@ class Car:
 class Niva(Car):
     pass
 
-
-engine = input('Enter the car engine: ')
-doorsCount = input('Enter the car doors count: ')
-color_car = str(input('Enter the car color: '))
-mycar = Niva(engine, doorsCount, color_car)
-# mycar.set_engine(engine)
-# mycar.set_doorsCount(doorsCount)
-# mycar.set_color(color_car)
-print("The car engine is: ", mycar.get_engine())
-print("The car doors count is: ", mycar.get_doorsCount())
-print("The car color is: ", mycar.get_color())
+def main():
+    engine = input('Enter the car engine: ')
+    doorsCount = int(input('Enter the car doors count: '))
+    color_car = str(input('Enter the car color: '))
+    color_car1 = color_car.isdigit()
+    print (color_car1)
+    if (color_car1 == True ):
+        print("Pleace write Red or Blue or green ...")    
+    else:
+        mycar = Niva(engine, doorsCount, color_car)
+        print("The car engine is: ", mycar.get_engine())
+        print("The car doors count is: ", mycar.get_doorsCount())
+        print("The car color is: ", mycar.get_color())
+        
+main()    
