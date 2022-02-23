73c2de39e9becb2e951b5adb21bf39bc8f1589f2
musho04 <mushegh.esayan.im@gmail.com>
2022-02-23 23:10:35 +0400

    Incomplete

---
+++ Python/Homework23.02.2022/best_calculiator.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#ekar chtarapelu hamar 
+python3
---
+++ Python/Homework23.02.2022/calculiator.py
@@ -0,0 +1,11 @@
+#calculiator
+#porcel em tarber lucumner () gorcoxutyuber katarelu hamar
+#mtacum em vor petq e unenaq inch vor mi zangvac u drsic ep or klcnenq inq@ 
+#arden kardalov zangvac@ haskna inch ene elementneri het
+#xndir drsic () het gorcoxutyun eneln e
+a = int(input("pakagcic dus tiv: "))
+c = int(input("pakagceri mej tiv1: "))
+e = int(input("pakagceri mej tiv1: "))
+
+i = a+(c*e)
+print (i)
