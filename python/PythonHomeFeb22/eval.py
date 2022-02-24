9917a594ecff3a3e810df792290c538e0081ab36
Khachik <you@example.com>
2022-02-24 12:23:20 +0400

    chenjis

--- python/PythonHomeFeb22/eval.py
+++ python/PythonHomeFeb22/eval.py
@@ -1,12 +1,17 @@
+        
 def myEval(str):
-    isTrue=True
-    while isTrue:
-        isTrue=False
-        arr = list(str)
+    open=0
+    close=0
+    for i in str:
+        if i == "(":
+            open +=1
+        elif i == ")":
+            close += 1   
+        elif close == open:
+            arr = list(str)
         for i in range(0, len(arr)):
             if arr[i] == "+":
                res=int(arr[i-1]) + int(arr[i+1])
-               isTrue=True
-               return res
-            
-print(myEval("1+4+9"))
+               return res+int(arr[i+3])
+
+print(myEval("5+5+5"))               
