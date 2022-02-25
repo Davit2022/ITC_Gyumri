accb02e14fc8784ebb3c482da212c9c6aaf8e666
TigranAlexanyan <alex.yan.19@mail.ru>
2022-02-25 13:07:32 +0400

    homework Python

--- python/python_8/matrix.py
+++ python/python_8/matrix.py
@@ -1,46 +1,46 @@
-def printMatrix(n):
-    arr = []   
-    for i in range(n):   
-        row = []  
-        for j in range(n): 
-            num = int(input("Enter the matrix element: "))  
-            row.append(num) 
-        arr.append(row) 
-    return arr    
-       
-    
-def primaryDiagonal(n):
-    data = printMatrix(n)   
+def printMatrix():
+    n = int(input("Enter number create to matrix: "))
+    arr = []
+    for i in range(n):
+        row = []
+        for j in range(n):
+            row.append(int(input("Enter the matrix element: ")))
+        arr.append(row)
+    for i in range(n):
+        for j in range(n):
+            print(arr[i][j], end=" ")
+        print()
+    return arr
+
+
+def primaryDiagonal(data):
     for i in range(len(data)):
         for j in range(len(data)):
             if i == j:
-                print(data[i][j])
+                print(data[i][j], end='')
+            else:
+                print(" ", end='')
+        print("")
 
-def secondaryDiagonal(n):
-    data = printMatrix(n)
+
+def secondaryDiagonal(data):
     for i in range(len(data)):
         for j in range(len(data)):
             if i + j == len(data) - 1:
                 print(data[i][j])
+            else:
+                print(" ", end='')
+        print("")
 
-def option(n):
+
+def option(data):
     num = int(input("Enter number 1 and get primary diagonal or 2 and get second diagonal: "))
     if num == 1:
-        primaryDiagonal(n)
-    elif num == 2:    
-        secondaryDiagonal(n)
-    
+        primaryDiagonal(data)
+    elif num == 2:
+        secondaryDiagonal(data)
+
 
 if __name__ == "__main__":
-    num = int(input("Enter number create to matrix: "))
-    data = printMatrix(num)
-    for i in range(len(data)):
-        for j in range(len(data)):
-            print(data[i][j], end=" ") 
-        print("\n")
-    option(num)
-
-
-    
-            
-        
+    data = printMatrix()
+    option(data)
