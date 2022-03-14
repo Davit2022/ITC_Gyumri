94f9bcae10b8513de4b3716039914136f0698beb
Khachik <you@example.com>
2022-03-15 01:24:25 +0400

    hw js login

--- javaScript/login/main.js
+++ javaScript/login/main.js
@@ -1,8 +1,9 @@
-let btn = document.querySelector(".btn")
-let show = document.querySelector(".open")
+const btn = document.querySelector(".btn")
+const show = document.querySelector(".open")
+
 
 show.addEventListener("click",()=>{
-   let window = document.querySelector(".window")
+   const window = document.querySelector(".window")
         if (window.style.display === "none") {
             window.style.display = "block";
         } else {
@@ -11,10 +12,38 @@ show.addEventListener("click",()=>{
 })
 
 btn.addEventListener("click",()=>{
-     let name = document.querySelector(".name").value;
-        console.log(name)
-     let email = document.querySelector(".email").value;
-         console.log(email)
-     let password = document.querySelector(".password").value;
-         console.log(password)
+     const name = document.querySelector(".name").value
+     const nameRgex = /^[a-zA-Z]*$/
+     const isValidName = nameRgex.test(name)
+   
+      if(isValidName == false){
+         console.log("Please enter a valid name")
+      }else{
+         console.log("ok")
+      }
+     const email = document.querySelector(".email").value
+     const emailRgex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+     const isValidEmail = emailRgex.test(email)
+
+      if(isValidEmail == false){
+         console.log("Please enter a valid email")
+      }else{
+         console.log("ok")
+   }
+     const password = document.querySelector(".password").value
+     const passwordRgex = /^[A-Za-z]\w{7,14}$/
+     const isValidPassword = passwordRgex.test(password)
+
+      if(isValidPassword == false){
+         console.log("Please enter a valid password")
+      }else{
+         console.log("ok")
+       }
+      const pass2 = document.querySelector("#pass2").value
+      if(pass2 === password){
+         console.log("ok")
+      }else{
+         console.log("Please enter a valid password")
+       }
+        
 })
