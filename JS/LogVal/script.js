81dea6d0e2a0bd8034cb416a83308565a3bd671d
SahakyanHayk <sahakyanhayk1991@gmail.com>
2022-03-15 23:03:01 +0400

    js val

--- JS/LogVal/script.js
+++ JS/LogVal/script.js
@@ -9,17 +9,6 @@ form.addEventListener('submit', e => {
     validateInputs();
 });
 
-function specialcharecter(){
-    var Characters = "!`#$%^&*()+=[]\\\';,./{}|\":<>?~";   
-    var data = document.getElementById("password").value;
-    for (var i = 0; i < data.length; i++){      
-        if (Characters.indexOf(data.charAt(i)) != -1){    
-            document.getElementById("password").value = "";
-            return false; 
-        } 
-    }
-}
-specialcharecter();
 const setError = (element, message) => {
     const inputControl = element.parentElement;
     const errorDisplay = inputControl.querySelector('.error');
@@ -38,10 +27,13 @@ const setSuccess = element => {
 };
 
 const validateInputs = () => {
+
     const usernameValue = username.value.trim();
     const lastnameValue = lastname.value.trim();
     const passwordValue = password.value.trim();
     const password2Value = password2.value.trim();
+    const psw=  /^[A-Za-z]\w{6,10}$/;
+    const isValidPsw = psw.test(passwordValue)
 
     if(usernameValue === '') {
         setError(username, 'Username is required');
@@ -58,10 +50,10 @@ const validateInputs = () => {
     if(passwordValue === ''){
         setError(password, 'Password is required');
     } else if (passwordValue.length < 8 ){
-        setError(password, 'Password must be at least 8 character.')
-    } else if(!specialcharecter()){
-        setError(password, 'Password has not allowed symbols')
-    }else {
+        setError(password, 'Password must be at least 8 character.');
+    }else if(!isValidPsw){
+        setError(password, 'Only characters, numbers, \'_\', first character-letter, 6-10 symbols');
+    }else{
         setSuccess(password);
     }
 
