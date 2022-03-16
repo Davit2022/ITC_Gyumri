3b0ac9f6ae07919298ba54cdada8b4cee6cd3f6d
Khachik <you@example.com>
2022-03-17 03:00:54 +0400

    hw

--- javaScript/login/main.js
+++ javaScript/login/main.js
@@ -2,7 +2,11 @@ const btn = document.querySelector(".btn")
 const show = document.querySelector(".open")
 
 
-show.addEventListener("click",()=>{
+const EVENTS_TYPES = {
+   CLICK : 'click',
+}
+
+show.addEventListener(EVENTS_TYPES.CLICK,()=>{
    const window = document.querySelector(".window")
         if (window.style.display === "none") {
             window.style.display = "block";
@@ -11,45 +15,39 @@ show.addEventListener("click",()=>{
         }
 })
 
-btn.addEventListener("click",()=>{
+btn.addEventListener(EVENTS_TYPES.CLICK,()=>{
    
      
      const name = document.querySelector(".name").value
      const nameRgex = /^[a-zA-Z]*$/
      const isValidName = nameRgex.test(name)
      const nameError = document.querySelector(".nameError")
+      nameError.style.display = isValidName == false || name === "" ? 'block' : 'none'
+     //  const setName = localStorage.setItem(name, name);
+     const getName = localStorage.getItem(name)
      
-      if(isValidName == false || name === ""){
-         nameError.style.display = "block"
-      }else{
-         nameError.style.display = "none"
-      }
+     const path = document.querySelector(".btn")
+    
+          if (getName === name && name !="") {
+               path.setAttribute("href","file:///home/khachik/gitHub/ITC_Gyumri/javaScript/login/table.html") 
+             
+          }else{
+               path.setAttribute("href","#")
+          }
+
      const email = document.querySelector(".email").value
      const emailRgex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
      const isValidEmail = emailRgex.test(email)
      const emailError = document.querySelector(".emailError")
-
-      if(isValidEmail == false){
-         emailError.style.display = "block"
-      }else{
-         emailError.style.display = "none"
-   }
+      emailError.style.display = isValidEmail == false ? 'block' : 'none'
+     
      const password = document.querySelector(".password").value
      const passwordRgex = /^[A-Za-z]\w{7,14}$/
      const isValidPassword = passwordRgex.test(password)
      const passwordError = document.querySelector(".passworError")
-
-      if(isValidPassword == false){
-          passwordError.style.display = "block"
-      }else{
-          passwordError.style.display = "none"
-       }
+       passwordError.style.display = isValidPassword == false ? 'block' : 'none'
+     
       const pass2 = document.querySelector("#pass2").value
       const passwordError2=document.querySelector(".passwordError2")
-      if(pass2 === password){
-         passwordError2.style.display = "none"
-      }else{
-         passwordError2.style.display = "blok"
-       }
-        
+       passwordError2.style.display = pass2 === password ? 'none' : 'block' 
 })
