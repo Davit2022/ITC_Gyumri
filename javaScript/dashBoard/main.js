e41769caf4fbdb5447e1abac68b863a88a3854d8
Khachik <you@example.com>
2022-03-24 23:36:14 +0400

    dashBoard

--- javaScript/dashBoard/main.js
+++ javaScript/dashBoard/main.js
@@ -2,7 +2,26 @@ const App = ()=>{
     const EVENTS_TYPES = {
         CLICK: "click",
         };
-   
+
+Promise.all([
+  fetch("https://corona-api.com/countries/am"),
+  fetch("https://corona-api.com/countries/ru"),
+  fetch("https://corona-api.com/countries/us")
+]).then((streams)=>{
+  return Promise.all(streams.map((stream)=> stream.json()));
+}).then((results)=>{
+    console.log(results[0].data.name)
+  get (results)
+});   
+
+function get(data){
+  const am = document.querySelector("#am")
+  const ru = document.querySelector("#ru")
+  const us = document.querySelector("#us")
+  am.innerHTML= data[0].data.name
+  ru.innerHTML= data[1].data.name
+  us.innerHTML= data[2].data.name
+}
 
 const btn = document.querySelectorAll(".btn")
 
