ef6154228daf08018328a273c6a9e9ad1dd733d1
Nvard <nvard.badalyan.im@gmail.com>
2022-03-29 17:57:45 +0400

    LightBox

---
+++ JS/Git Hub tasks*/LightBox/main.js
@@ -0,0 +1,61 @@
+const App = ()=>{
+
+function draw(data) {
+  const wrap = document.getElementsByClassName("row")
+  console.log(wrap[0])
+  for (let i = 0; i < 5; i++) {
+    const div = document.createElement("div")
+
+    wrap[0].append(div)
+  }
+}
+
+draw()
+
+
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function openLightbox() {
+  document.getElementById('Lightbox').style.display = 'block';
+}
+
+function closeLightbox() {
+  document.getElementById('Lightbox').style.display = 'none';
+}
+
+function changeSlide(n) {
+	showSlide(slideIndex += n);
+}
+
+function toSlide(n) {
+	showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+
+  const slides = document.getElementsByClassName('slide');
+  let modalPreviews = document.getElementsByClassName('modal-preview');
+
+  if (n > slides.length) {
+    slideIndex = 1;	
+  }
+  
+  if (n < 1) {
+  	slideIndex = slides.length;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (let i = 0; i < modalPreviews.length; i++) {
+      modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
+  }
+  
+  slides[slideIndex - 1].style.display = 'block';
+  modalPreviews[slideIndex - 1].className += ' active';
+}
+}
+
+
+window.document.addEventListener("DOMContentLoaded",App)
