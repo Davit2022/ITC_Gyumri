610cc8fceabedd547f0c96a6b340fd82a1b5573f
Hayk2022 <hayk.sahakyan.im@gmail.com>
2022-04-07 17:39:57 +0400

    node js

---
+++ JS/NodeJS/index.js
@@ -0,0 +1,20 @@
+// import path from "path";
+// import {createServer} from "http";
+// const server = createserver((req, res) => {
+//     res.writeHead(200, {"Content-Type": "text/plain"});
+//     res.end("Hayk");
+// })
+// server.listen(3001);
+
+
+// const http = require("http")
+// const fs = require("fs")
+
+// const server = http.createServer((req, res) => {
+//     fs.readFile("index.html", (err, data) => {
+//         res.writeHead(200, { 'Content-Type': 'text/html' })
+//         res.write(data)
+//         res.end()
+//     })
+// })
+// server.listen(8080)
