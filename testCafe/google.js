ed403412e555660572092c37a6dbdaee87d44fd1
Khachik <you@example.com>
2022-05-13 01:20:40 +0400

    testcafe

---
+++ testCafe/google.js
@@ -0,0 +1,14 @@
+import {Selector} from "testcafe"
+
+fixture `Google test`
+.page `https://www.microsoft.com/`;
+const serchResult = Selector('#coreui-feature-4icuog0 > section > div > h2')
+
+test('test.1',async t =>{
+    await t
+     .click('#search')
+    .typeText('#cli_shellHeaderSearchInput','word')
+    .pressKey('enter')
+    .expect(serchResult.innerText).contains('Microsoft 365')
+    
+})
